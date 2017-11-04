f09c775ca45ec2966cf3c2ffb42b101ef9ae1dbd
unknown <damisv@gmail.com>
2017-11-04 13:46:58 +0200

    Firebase set up

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import * as dotenv from 'dotenv'
 import {App} from './config/express'
+import {FireBaseAdmin} from './firebase/admin'
 
-const config = dotenv.config({path: `.env/${process.argv[2]}`}).parsed
+const config:any = dotenv.config({path: `.env/${process.argv[2]}`}).parsed
 const app = new App(config)
+const firebase = new FireBaseAdmin(config.FIREBASE_KEY_LOCATION)
 
 app.init()
 app.initServiceStatus()
